5d2eb9fd7ae7ed3f360df7ec05251802d5d7dae6
sudoberlin <shubham4163@nitnagaland.ac.in>
2020-02-16 18:44:17 +0530

    initial commit

---
+++ circular_llist.py/llist_to_cllist.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Circularllist:
+    def __init__(self):
+        self.head = None
+
+    def push (self, data):
+        new_node = Node(data)
+        temp = self.head
+
+        new_node.next = self.head
+
+        if self.head != None:
+            while(temp.next != self.head):
+                temp = temp.next
+
+            temp.next = new_node
+
+        else:
+            new_node.next = new_node
+
+        self.head = new_node
+
+
+    def tocircular(self, head):
+        start = head
+
+        while (head.next != None):
+            head = head.next
+
+        head.next = start
+        return start
+    
+    def printcllist(self):
+        temp = self.head
+        if self.head != None:
+            while(True):
+                print(temp.data)
+                temp = temp.next
+                if (temp == self.head):
+                    break
+
+
+
+cllist = Circularllist()
+
+cllist.push(1)
+cllist.push(2)
+cllist.push(3)
+cllist.push(9)
+
+cllist.printcllist()
+
